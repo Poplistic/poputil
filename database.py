18c98112c055852a19cf5efd78e46ae38ab6d6c0
Poplistic <poplisticcreations@gmail.com>
2026-01-10 15:06:59 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,25 @@
 import sqlite3
 
-db = sqlite3.connect("poputil.db")
+db = sqlite3.connect("poputil.db", check_same_thread=False)
 cursor = db.cursor()
 
+# Economy table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS economy (
     user_id INTEGER,
     guild_id INTEGER,
-    balance INTEGER,
+    balance INTEGER DEFAULT 0,
+    last_daily INTEGER DEFAULT 0,
     PRIMARY KEY (user_id, guild_id)
 )
 """)
 
+# Server config table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS config (
     guild_id INTEGER PRIMARY KEY,
     welcome_channel INTEGER,
-    autorole INTEGER,
-    log_channel INTEGER
+    autorole INTEGER
 )
 """)
 
